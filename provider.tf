df07b369547731ea8b3d6b5502f281fa8da2fe41
Escael1 <ju.aquebeque@duocuc.cl>
2026-04-03 22:09:54 -0300

    feat: infraestructura base Terraform (VPC 10.1.0.0/16, SG, EC2 Ubuntu 24.04)

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  # Esta es la versión del programa Terraform que tienes instalado
+  required_version = ">= 1.14.8" 
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      # Esta es la versión del "traductor" de AWS (HashiCorp)
+      version = "~> 5.0" 
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
